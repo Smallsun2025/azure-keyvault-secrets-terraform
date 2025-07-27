f376d32498775756d145d90c9fce6087b70fc12d
Smallsun2025 <lihongyang2025@gmail.com>
2025-07-27 22:43:05 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = var.key_vault_name
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = true
+  soft_delete_retention_days  = 7
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  name         = var.secret_name
+  value        = var.secret_value
+  key_vault_id = azurerm_key_vault.kv.id
+}
